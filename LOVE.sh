1e9c15b40e3054b884b558bfd4b54b8c11e134fa
SOUROV KHAN NAHID <67148243+SOUROVKHANNAHID@users.noreply.github.com>
2023-03-24 13:27:50 +0600

    Update LOVE.sh
    Now it'll automatically install required packages

--- LOVE.sh
+++ LOVE.sh
@@ -1,8 +1,8 @@
+pkg install figlet && pkg install toilet-y && clear
 figlet I
 figlet LOVE
 figlet YOU
-toilet -f mono12 -F gay "I"
-toilet -f mono12 -F gay "LOVE"
+toilet -f mono12 -F gay "I"toilet -f mono12 -F gay "LOVE"
 toilet -f mono12 -F gay "YOU"
 figlet I
 figlet LOVE
